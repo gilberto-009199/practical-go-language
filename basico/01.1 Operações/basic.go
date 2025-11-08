5334fd2c7f28261449261a1e77a05ea4fdaab004
gil <38070920+gilberto-009199@users.noreply.github.com>
2025-11-08 16:34:28 -0300

    Update bit shifting calculations for storage units

--- basico/01.1 Operações/basic.go
+++ basico/01.1 Operações/basic.go
@@ -27,11 +27,11 @@ func main(){
 	fmt.Printf("# ²√%v	\t= %v \n", val2,  math.Sqrt(float64(val2))  )
 	// Deslogamento de bits
 
-	KB  := 1 <<  (1 * 10) // Operador << Desloga Bytes para a direita 
-	MB  := 1 <<  (2 * 10) // MB = KB deslogado para a direita 2 * 10
-	GB  := 1 <<  (3 * 10) // GB = MB deslogado para a direita 3 * 10
-	TB  := 1 <<  (4 * 10) // TB = GB deslogado para a direita 4 * 10
-
+	KB := 1 << (1 * 10)  // 1 deslocado 10 bits para esquerda = 2¹⁰ = 1024
+	MB := 1 << (2 * 10)  // 1 deslocado 20 bits para esquerda = 2²⁰ = 1.048.576
+	GB := 1 << (3 * 10)  // 1 deslocado 30 bits para esquerda = 2³⁰ = 1.073.741.824
+	TB := 1 << (4 * 10)  // 1 deslocado 40 bits para esquerda = 2⁴⁰ = 1.099.511.627.776
+		
 	fmt.Println("Binarios : ")
 	fmt.Printf(" KB : %b\n",KB)
 	fmt.Printf(" MB : %b\n",MB)
